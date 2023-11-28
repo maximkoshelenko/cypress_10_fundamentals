50dcc3cead8cd6e0883aa792bafd342607e3324b
Maksim Koshelenko <maxim.koshelenko@gmail.com>
2023-11-28 20:45:29 +0200

    update some tests

--- cypress/e2e/play.cy.ts
+++ cypress/e2e/play.cy.ts
@@ -13,6 +13,14 @@ describe('Sudoku', () => {
         (window as CustomWindow).starting = starting;
         (window as CustomWindow).solved = solved;
       },
+    });
+    cy.get('.game__cell:contains(0)').should('have.length', 3);
+    starting.forEach((cell, index) => {
+      if (cell === '0') {
+        cy.get('.game__cell').eq(index).click();
+        cy.contains('.status__number', solved[index]).click();
+      }
     })
+    cy.contains('.overlay__text', 'You solved it!').should('be.visible');
   })
 })
