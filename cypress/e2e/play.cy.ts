a7d5c81b7405bb0b849941f8266be82b7cc09f87
Maksim Koshelenko <maxim.koshelenko@gmail.com>
2023-11-28 18:38:05 +0200

    add new tests

---
+++ cypress/e2e/play.cy.ts
@@ -0,0 +1,20 @@
+import { starting, solved } from '../fixtures/sudoku.json'
+
+// Extend the Window interface with custom properties
+interface CustomWindow extends Window {
+  starting?: string[];
+  solved?: string[];
+}
+
+describe('Sudoku', () => {
+  it('Play the same game', () => {
+
+    cy.visit('/', {
+      onBeforeLoad: (window) => {
+        (window as CustomWindow).starting = starting;
+        (window as CustomWindow).solved = solved;
+      },
+    })
+
+  })
+})
